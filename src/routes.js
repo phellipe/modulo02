04454ca70eb3f230d65acf764e4d03371fde9018
João Phellipe de Freitas Pinto <phellipe@ufersa.edu.br>
2019-08-20 14:21:33 -0300

    created model loader

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Phellipe!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'João Phellipe',
+    email: 'phellipe@ufersa.edu.br',
+    password_hash: '123456789',
+  });
+  return res.json({ user });
 });
 
 export default routes;
